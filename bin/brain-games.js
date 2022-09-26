ca23cf52fafca4244005c9e82dbd73dbbe7658ec
ElisabethFox <lisa.black.fox17@gmail.com>
2022-09-27 01:16:55 +0300

    brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node.
 
-import { getTheUserName } from "../src/cli.js";
+import * as greetingTheUser from "../src/cli.js";
 
-console.log("Welcome to the Brain Games!");
-console.log(`Hello, ${getTheUserName()}!`);
+greetingTheUser;
