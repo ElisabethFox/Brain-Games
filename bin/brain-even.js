be087746ac8ea4ea0204fd74f342b32bad852fd0
ElisabethFox <lisa.black.fox17@gmail.com>
2022-09-27 01:00:23 +0300

    edit brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node.
 import readlineSync from "readline-sync";
-import * as greetingTheUser from "../bin/brain-games.js";
-import { getTheUserName } from "../src/cli.js";
+import * as greetingTheUser from "../src/cli.js";
+import { userName } from "../src/cli.js";
 
 greetingTheUser;
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
@@ -19,17 +19,17 @@ const isTheNumberEven = () => {
     const question = `Question: ${randomInteger}`;
     console.log(question);
     const answer = readlineSync.question("Your answer: ");
-    if (randomInteger % 2 === 0 && answer === "yes") {
-      console.log("Correct!");
-      i += 1;
-    } else if (randomInteger % 2 !== 0 && answer === "no") {
+    if (
+      (randomInteger % 2 === 0 && answer === "yes") ||
+      (randomInteger % 2 !== 0 && answer === "no")
+    ) {
       console.log("Correct!");
       i += 1;
     } else {
-      return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, userName!`;
+      return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`;
     }
   }
-  console.log(`Congratulations, userName!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 console.log(isTheNumberEven());
