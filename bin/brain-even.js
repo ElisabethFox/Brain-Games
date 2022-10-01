c447f5afdb9fac88551010e368df032a235df217
ElisabethFox <lisa.black.fox17@gmail.com>
2022-10-01 13:33:35 +0300

    complete the brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,14 +2,10 @@
 import readlineSync from "readline-sync";
 import * as greetingTheUser from "../src/cli.js";
 import { userName } from "../src/cli.js";
+import { getRandomInteger } from "../bin/getRandomInteger.js";
 
 greetingTheUser;
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
-
-const getRandomInteger = (min = 0, max = 100) => {
-  const integer = min + Math.random() * (max + 1 - min);
-  return Math.floor(integer);
-};
 
 const isTheNumberEven = () => {
   let i = 0; // счетчик верных ответов
@@ -35,5 +31,3 @@ const isTheNumberEven = () => {
 };
 
 console.log(isTheNumberEven());
-
-export { getRandomInteger };
