feb252ab3dc91cec7f62cc138ba540ff8924548b
ElisabethFox <lisa.black.fox17@gmail.com>
2022-10-05 18:01:32 +0300

    complete the brain-even game executable file

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,4 @@
 #!/usr/bin/env node
-import playTheGame from "../games/even.js";
+import playTheGame from "../src/games/even.js";
 
 playTheGame();
-
-// import readlineSync from "readline-sync";
-// import * as greetingTheUser from "../src/cli.js";
-// import { userName } from "../src/cli.js";
-// import { getRandomInteger } from "../src/getRandomInteger.js";
-
-// const isTheNumberEven = () => {
-//   greetingTheUser;
-//   const task = 'Answer "yes" if the number is even, otherwise answer "no"';
-//   console.log(task);
-
-//   let i = 0; // счетчик верных ответов
-
-//   while (i < 3) {
-//     const randomInteger = getRandomInteger(0, 100);
-//     const question = `Question: ${randomInteger}`;
-//     console.log(question);
-//     const answer = readlineSync.question("Your answer: ");
-//     const correctAnswer = randomInteger % 2 === 0 ? "yes" : "no";
-
-//     if (answer === correctAnswer) {
-//       console.log("Correct!");
-//       i += 1;
-//     } else {
-//       return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`;
-//     }
-//   }
-
-//   return `Congratulations, ${userName}!`;
-// };
-
-// console.log(isTheNumberEven());
