58707920c942dc289218f76893c717923fb8a1b6
ElisabethFox <lisa.black.fox17@gmail.com>
2022-10-06 17:46:41 +0300

    complete the general logic of all games

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import readlineSync from "readline-sync";
-import * as greetingTheUser from "../src/cli.js";
-import { userName } from "../src/cli.js";
+import readlineSync from 'readline-sync'
 
 const playTheGame = (task, getQuestionAndAnswer) => {
-  greetingTheUser;
-  console.log(task);
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log(task)
 
   for (let i = 0; i < 3; i += 1) {
-    const [question, correctAnswer] = getQuestionAndAnswer();
-    console.log(`Question: ${question}`);
-    const answer = readlineSync.question("Your answer: ");
+    const [question, correctAnswer] = getQuestionAndAnswer()
+    console.log(`Question: ${question}`)
+    const answer = readlineSync.question('Your answer: ')
 
     if (answer === correctAnswer) {
-      console.log("Correct!");
+      console.log('Correct!')
     } else {
       return console.log(
         `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`
-      );
+      )
     }
   }
-  return console.log(`Congratulations, ${userName}!`);
-};
+  return console.log(`Congratulations, ${userName}!`)
+}
 
-export { playTheGame };
+export { playTheGame }
