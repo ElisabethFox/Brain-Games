fd325900737bc3eafc313e3b176a425e8a54240d
ElisabethFox <lisa.black.fox17@gmail.com>
2022-10-11 23:28:38 +0300

    create function isEven

--- src/games/even.js
+++ src/games/even.js
@@ -1,15 +1,17 @@
 import getRandomInteger from '../helper.js';
 import playTheGame from '../index.js';
 
-const task = 'Answer "yes" if the number is even, otherwise answer "no"';
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
+
+const isEven = number => number % 2 === 0;
 
 const getQuestionAndAnswer = () => {
   const question = getRandomInteger(0, 100);
-  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
 
 export default () => {
-  playTheGame(task, getQuestionAndAnswer);
+  playTheGame(description, getQuestionAndAnswer);
 };
