febedc0719813d655dab01bf9fc7b826ef45dfe3
ElisabethFox <lisa.black.fox17@gmail.com>
2022-10-11 23:27:09 +0300

    delete function getRamdomExpression, edit function getResultOfExpression

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,43 +1,39 @@
 import getRandomInteger from '../helper.js';
 import playTheGame from '../index.js';
 
-const task = 'What is the result of the expression?';
+const description = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
 
-const getRandomExpression = (operators = ['+', '-', '*']) => {
-  const randomIndex = Math.floor(Math.random() * operators.length);
-  const randomOperator = operators[randomIndex];
+const getResultOfExpression = (firstOperand, secondOperand, randomOperator) => {
+  let result = 0;
 
-  const firstOperand = getRandomInteger(0, 10);
-  const secondOperand = getRandomInteger(0, 10);
-
-  return `${firstOperand} ${randomOperator} ${secondOperand}`;
-};
-
-const resultOfExpression = (expression) => {
-  const elements = expression.split(' ');
-  let result = Number(elements[0]);
-
-  switch (elements[1]) {
+  switch(randomOperator) {
     case '+':
-      result += Number(elements[2]);
+      result = firstOperand + secondOperand;
       break;
     case '-':
-      result -= Number(elements[2]);
+      result = firstOperand - secondOperand;
       break;
-    default:
-      result *= Number(elements[2]);
+    case '*':
+      result = firstOperand * secondOperand;
+    break;
   }
 
   return result;
 };
 
 const getQuestionAndAnswer = () => {
-  const question = getRandomExpression();
-  const correctAnswer = String(resultOfExpression(question));
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  const randomOperator = operators[randomIndex];
+  const firstOperand = getRandomInteger(0, 10);
+  const secondOperand = getRandomInteger(0, 10);
+
+  const question = `${firstOperand} ${randomOperator} ${secondOperand}`;
+  const correctAnswer = String(getResultOfExpression(firstOperand, secondOperand, randomOperator));
 
   return [question, correctAnswer];
 };
 
 export default () => {
-  playTheGame(task, getQuestionAndAnswer);
+  playTheGame(description, getQuestionAndAnswer);
 };
